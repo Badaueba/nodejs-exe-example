91cf152dd2fc15004ca19efadc2b2ecdc431c4cc
badaueba <alan.alalabadaueba@hotmail.com>
2023-03-18 08:24:21 -0300

    changing prompt

--- first-module.ts
+++ first-module.ts
@@ -3,16 +3,49 @@
 import figlet = require("figlet");
 import fs = require("fs");
 import path = require("path");
+import enquirer = require("enquirer");
+import clear = require("clear");
+import chalk = require("chalk");
 
-export = function firstModule(name: string): void {
+interface EnquirerPrompt<T> {
+    answer: T;
+}
+
+export = async function firstModule(name: string) {
     const fontData = fs.readFileSync(
-        path.join(__dirname, "assets/fonts/ANSI_Regular.flf"),
+        path.join(__dirname, "/assets/fonts/ANSI_Regular.flf"),
         "utf-8"
     );
-    figlet.parseFont("ANSI Regular", fontData);
-    const text = figlet.textSync(`hello ${name}`, {
-        font: "ANSI Regular",
-        horizontalLayout: "fitted",
+
+    clear({
+        fullClear: true,
     });
-    console.log(text);
+
+    const fontName = "Basic";
+    figlet.parseFont(fontName, fontData);
+
+    let text = figlet.textSync(`hello ${name || "person"}`, {
+        font: fontName,
+        width: 120,
+        horizontalLayout: "full",
+        verticalLayout: "controlled smushing",
+    });
+
+    console.log(chalk.red(text));
+
+    const firstQuestion: EnquirerPrompt<string> = await enquirer.prompt({
+        message: "What is your favorite color?",
+        type: "input",
+        name: "answer",
+    });
+
+    text = figlet.textSync(`${firstQuestion.answer}, Great!`, {
+        font: fontName,
+    });
+
+    chalk[firstQuestion.answer]
+        ? console.log(chalk[firstQuestion.answer](text))
+        : console.log(chalk.grey(text));
+
+    setTimeout(() => {}, 10_000);
 };
